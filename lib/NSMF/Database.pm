b4f804c9198a230f9aec59be2b47c210da98ef43
Eduardo Urias <windkaiser@gmail.com>
2012-01-03 18:31:49 -0700

    Added: Database Mock and Server initial implementation

---
+++ lib/NSMF/Database.pm
@@ -0,0 +1,28 @@
+package Database;
+
+use strict;
+use 5.010;
+
+#use EV;
+use AnyEvent;
+use Data::Dumper;
+
+sub new {
+    bless {}, __PACKAGE__
+}
+
+sub search {
+    my ($self, $params) = @_;
+
+    for my $source (keys %$params) {
+        say "Searching $source";
+        say Dumper $params->{$source};
+    }
+}
+
+sub sleep {
+    my ($self, $cb) = @_;
+    my $w; $w = AE::timer 3, 0, sub { $cb->(); undef $w };   
+}
+
+1;
